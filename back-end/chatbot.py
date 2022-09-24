4859c5a4400b024f27ffcadc8251dceb8a2d48c4
Matthew Turner <matur@microsoft.com>
2022-09-24 19:23:39 +0000

    latest

--- back-end/chatbot.py
+++ back-end/chatbot.py
@@ -16,7 +16,7 @@ class Chatbot:
         self.sender_number = sender_number
         self.client = Client(account_sid, auth_token)
 
-    def sendMessage(phone_number, message)
+    def sendMessage(phone_number, message):
         return client.messages.create(
                 body='This is the ship that made the Kessel Run in fourteen parsecs?',
                 from_=self.sender_number,
