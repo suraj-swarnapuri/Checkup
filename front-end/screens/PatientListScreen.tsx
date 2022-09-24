b82aa99f4cbd59920008c76bd4fd69cc95302a36
Marshall-Artz <marshallarts12@gmail.com>
2022-09-24 11:34:57 -0500

    material core

--- front-end/screens/PatientListScreen.tsx
+++ front-end/screens/PatientListScreen.tsx
@@ -1,10 +1,36 @@
 
 import React from "react"
-import { Text, Button, View, SafeAreaView, StyleSheet } from "react-native";
+import { Text, Button, View, StyleSheet } from "react-native";
+import { ListItem, Avatar } from "@react-native-material/core";
 
 const PatientListScreen = ({ navigation }) => {
     return (
-        <View style={styles.test}>
+        <>
+            <ListItem
+            leadingMode="avatar"
+            leading={
+                <Avatar image={{ uri: "https://mui.com/static/images/avatar/1.jpg" }} />
+            }
+            title="Brunch this weekend?"
+            secondaryText="I'll be in your neighborhood doing errands this…"
+            />
+            <ListItem
+            leadingMode="avatar"
+            leading={
+                <Avatar image={{ uri: "https://mui.com/static/images/avatar/2.jpg" }} />
+            }
+            title="Summer BBQ"
+            secondaryText="Wish I could come, but I'm out of town this…"
+            />
+            <ListItem
+            leadingMode="avatar"
+            leading={
+                <Avatar image={{ uri: "https://mui.com/static/images/avatar/3.jpg" }} />
+            }
+            title="Oui Oui"
+            secondaryText="Do you have Paris recommendations? Have you ever…"
+            />
+            <View style={styles.test}>
             <Text>Patient List Screen</Text>
             <Button
                 title="Go to Detail"
@@ -12,7 +38,8 @@ const PatientListScreen = ({ navigation }) => {
                     navigation.navigate('PatientDetail', { name: 'test' })
                 }
             />
-        </View>
+            </View>
+        </>
     );
 }
 
