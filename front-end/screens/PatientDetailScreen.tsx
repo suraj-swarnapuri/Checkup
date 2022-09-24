8e95df1185a303d1b253546025891363800e3379
Sunil Mac <sunil.sampath@shopsequor.com>
2022-09-24 14:19:35 -0500

    got init data showing

--- front-end/screens/PatientDetailScreen.tsx
+++ front-end/screens/PatientDetailScreen.tsx
@@ -1,41 +1,67 @@
 
+import { Stack, Chip } from "@react-native-material/core";
 import React from "react"
 import { Text, SafeAreaView, View, StyleSheet } from "react-native";
-import { Icon } from 'react-native-elements'
+import { Divider, Icon } from 'react-native-elements'
 function PatientDetailScreen(props: any) {
     return (
         <SafeAreaView>
-            <View style={styles.row}>
-                <View style={styles.col}>
-                    <Text>Pulse</Text>
+            <Stack>
+                <View style={styles.row}>
+                    <View style={styles.col}>
+                        <Icon style={styles.icon} name={"heart-outline"} size={60} type="ionicon" tvParallaxProperties={undefined} />
+
+                        <Text style={styles.subtitle}>96 BPM</Text>
+                    </View>
+                    <View style={styles.col}>
+                        <Icon style={styles.icon} name={"water-outline"} size={60} type="ionicon" tvParallaxProperties={undefined} />
+                        <Text style={styles.subtitle}>130/90</Text>
+                    </View>
                 </View>
-                <View style={styles.col}>
-                    <Text>BP</Text>
+                <View style={styles.row}>
+                    <View style={styles.col}>
+
+                        <Icon style={styles.icon} name={"pizza-outline"} size={60} type="ionicon" tvParallaxProperties={undefined} />
+                        <Text style={styles.subtitle}> 5 B/m</Text>
+                    </View>
+                    <View style={styles.col}>
+                        <Icon style={styles.icon} name={"thermometer-outline"} size={60} type="ionicon" tvParallaxProperties={undefined} />
+                        <Text style={styles.subtitle}>100.1 F</Text>
+                    </View>
                 </View>
-            </View>
-            <View style={styles.row}>
-                <View style={styles.col}>
-                    <Text>Respiration</Text>
-                </View>
-                <View style={styles.col}>
-                    <Text>Temp</Text>
-                </View>
-            </View>
-        </SafeAreaView >
+            </Stack>
+            <Divider></Divider>
+            <Text>Care and Prescriptions</Text>
+
+            <Divider></Divider>
+        </SafeAreaView>
     );
 }
 
 const styles = StyleSheet.create({
+    subtitle: {
+        fontWeight: "bold",
+        fontSize: 22,
+        textAlign: "center",
+
+    },
     row: {
         flexDirection: "row",
-        backgroundColor: "lightgrey"
+        backgroundColor: "#B0E2FF"
     },
     col: {
-        backgroundColor: "red",
+        backgroundColor: "#EBEBEB",
         flex: 1,
         height: 200,
         padding: 10,
-        margin: 10
+        margin: 10,
+        borderRadius: 10,
+        justifyContent: "center",
+        shadowRadius: 5
+    },
+    icon: {
+        alignItems: "center",
+        marginBottom: 10
     }
 })
 
