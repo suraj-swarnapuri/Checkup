3ac0d3a4e4eda9e97a8f65d45d433a49f093abeb
Sunil Mac <sunil.sampath@shopsequor.com>
2022-09-24 15:30:39 -0500

    finished bare bones styling

--- front-end/screens/PatientDetailScreen.tsx
+++ front-end/screens/PatientDetailScreen.tsx
@@ -1,14 +1,15 @@
 
-import { Stack, Chip } from "@react-native-material/core";
+import { Stack, ListItem, Avatar } from "@react-native-material/core";
 import React from "react"
 import { Text, SafeAreaView, View, StyleSheet } from "react-native";
-import { Divider, Icon } from 'react-native-elements'
+import { Icon } from 'react-native-elements'
 function PatientDetailScreen(props: any) {
     return (
         <SafeAreaView>
             <Stack>
-                <View style={styles.row}>
+                <View style={styles.section}>                <View style={styles.row}>
                     <View style={styles.col}>
+                        {/*  <PulseAnimation style={{ justifyContent: "center" }} color={'#aaa'} numPulses={1} diameter={100} speed={500} duration={2000} /> */}
                         <Icon style={styles.icon} name={"heart-outline"} size={60} type="ionicon" tvParallaxProperties={undefined} />
 
                         <Text style={styles.subtitle}>96 BPM</Text>
@@ -18,36 +19,73 @@ function PatientDetailScreen(props: any) {
                         <Text style={styles.subtitle}>130/90</Text>
                     </View>
                 </View>
-                <View style={styles.row}>
-                    <View style={styles.col}>
+                    <View style={styles.row}>
+                        <View style={styles.col}>
 
-                        <Icon style={styles.icon} name={"pizza-outline"} size={60} type="ionicon" tvParallaxProperties={undefined} />
-                        <Text style={styles.subtitle}> 5 B/m</Text>
-                    </View>
-                    <View style={styles.col}>
-                        <Icon style={styles.icon} name={"thermometer-outline"} size={60} type="ionicon" tvParallaxProperties={undefined} />
-                        <Text style={styles.subtitle}>100.1 F</Text>
+                            <Icon style={styles.icon} name={"pizza-outline"} size={60} type="ionicon" tvParallaxProperties={undefined} />
+                            <Text style={styles.subtitle}> 5 B/m</Text>
+                        </View>
+                        <View style={styles.col}>
+                            <Icon style={styles.icon} name={"thermometer-outline"} size={60} type="ionicon" tvParallaxProperties={undefined} />
+                            <Text style={styles.subtitle}>100.1 F</Text>
+                        </View>
                     </View>
                 </View>
-            </Stack>
-            <Divider></Divider>
-            <Text>Care and Prescriptions</Text>
+                <View style={styles.section}>
+                    <Text style={styles.title}>Care and Prescriptions</Text>
+                    <Text style={styles.listItem}>- 600mg of Ibuprofen every 6 hrs</Text>
+                    <Text style={styles.listItem}>- 100mg of Fentanyl every 12 hrs</Text>
+                    <Text style={styles.listItem}>- Monitor vitals after midnight</Text>
+                </View>
+                <View style={styles.section}>
+                    <Text style={styles.title}>Activity Log</Text>
+                    <ListItem
+                        trailing={
+                            <Text style={{ fontSize: 14 }}>11:59PM</Text>
+                        }
+                        title="Suraj S."
+                        secondaryText="Administered 2mg of dopamine"
+                    />
+                    <ListItem
 
-            <Divider></Divider>
+                        trailing={
+                            <Text style={{ fontSize: 14 }}>6:30PM</Text>
+                        }
+                        title="Suraj S."
+                        secondaryText="Administered 20mg of fentanyl"
+                    />
+                </View>
+            </Stack>
         </SafeAreaView>
     );
 }
 
 const styles = StyleSheet.create({
+    section: {
+        padding: 10,
+        margin: 10,
+        borderRadius: 10,
+        backgroundColor: "#B0E2FF"
+    },
+    title: {
+        marginBottom: 10,
+        fontSize: 25,
+        fontWeight: "bold",
+        textAlign: "center",
+
+    },
     subtitle: {
         fontWeight: "bold",
         fontSize: 22,
         textAlign: "center",
 
     },
+    listItem: {
+        fontSize: 18,
+        margin: 5
+    },
     row: {
-        flexDirection: "row",
-        backgroundColor: "#B0E2FF"
+        flexDirection: "row"
     },
     col: {
         backgroundColor: "#EBEBEB",
